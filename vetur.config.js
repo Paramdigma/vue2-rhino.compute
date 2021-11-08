d7c05b1520277dd7754607c6a2a5222e897cc7f9
Christian <christian.j.dimitrii@gmail.com>
2021-11-08 10:17:38 +0100

    jsconfig.json

--- vetur.config.js
+++ vetur.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // vetur configuration, which will override the settings in vscode.  default: `{}`
   settings: {
     "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
-  }
+    "vetur.experimental.templateInterpolationService": true,
+  },
   // Normal projects use the default configuration default: `[{ root: './' }]`
 };
