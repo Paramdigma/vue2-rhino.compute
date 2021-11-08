b85db7e16a91fcd56f0631d2795a303c1fbd5eb3
Christian <christian.j.dimitrii@gmail.com>
2021-11-08 14:39:53 +0100

    dotenv rhino config properties added

--- src/main.js
+++ src/main.js
@@ -12,14 +12,17 @@ Vue.config.productionTip = false;
 
 Vue.use(Buefy);
 
-window.rhino3dm().then(async (rhino) => {
+window.rhino3dm().then(async rhino => {
+  window.RhinoCompute.url = process.env.VUE_APP_RHINO_COMPUTE_URL;
+  window.RhinoCompute.apiKey = process.env.VUE_APP_RHINO_COMPUTE_API_KEY;
   Vue.prototype.$RhinoCompute = window.RhinoCompute;
   Vue.prototype.$rhino = rhino;
   Vue.prototype.$THREE = THREE;
   Vue.prototype.$OrbitControls = OrbitControls;
   Vue.prototype.$Rhino3dmLoader = Rhino3dmLoader;
+
   new Vue({
     router,
-    render: (h) => h(App),
+    render: h => h(App)
   }).$mount("#app");
 });
