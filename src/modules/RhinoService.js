d36d2100d366543aa9f4478dc0e3fa59b7243547
Christian <christian.j.dimitrii@gmail.com>
2021-11-10 18:37:43 +0100

    grasshopper parameter and rhino service modules added

--- src/modules/RhinoService.js
+++ src/modules/RhinoService.js
@@ -1,16 +1,17 @@
 export default function RhinoService() {}
 // // set auth token and url
-RhinoService.prototype.init = function () {
+RhinoService.prototype.init = function async() {
   // set auth token
   return new Promise((resolve, reject) => {
-    console.log("It is done.");
+    // console.log("auth successful");
 
     // Succeed half of the time.
     if (window.rhino3dm) {
       window.RhinoCompute.url = process.env.VUE_APP_RHINO_COMPUTE_URL;
       window.RhinoCompute.apiKey = process.env.VUE_APP_RHINO_COMPUTE_API_KEY;
       window.RhinoCompute.authToken = process.env.VUE_APP_RHINO_COMPUTE_API_KEY;
-      window.rhino3dm().then(rh => {
+
+      window.rhino3dm().then((rh) => {
         window.Rhino3dm = rh;
         resolve();
       });
@@ -21,10 +22,24 @@ RhinoService.prototype.init = function () {
   });
 };
 
-RhinoService.prototype.loadFileFromUrl = async function (url) {
-  let res = await fetch(url);
-  console.log("res:0", res);
+RhinoService.prototype.loadGrasshopperFileLocally = async function (filePath) {
+  let res = await fetch(filePath);
+  // console.log("res:0", res);
   let buffer = await res.arrayBuffer();
   var definition = new Uint8Array(buffer);
-  console.log("buffer: ", buffer, "definition: ", this.definition);
+  // console.log("buffer: ", buffer, "definition: ", definition);
+  return definition;
 };
+
+// rhino compute evalute definition
+// RhinoService.prototype.computeGrasshopperDefinition = async function (
+//   definition,
+//   trees
+// ) {
+//   const res = await window.RhinoCompute.Grasshopper.evaluateDefinition(
+//     definition,
+//     trees
+//   );
+//   console.log("computed grasshopper response", res);
+//   return res;
+// };
